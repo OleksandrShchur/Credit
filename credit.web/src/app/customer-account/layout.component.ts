bf6fec45b245b38a1fb7e638f8997aca3c02bfd1
oleksandr_shchur <osur12709@gmail.com>
2023-06-01 01:00:26 +0300

    add login/registration forms

---
+++ credit.web/src/app/customer-account/layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({ templateUrl: 'layout.component.html' })
+export class LayoutComponent {
+  constructor(private router: Router) {
+    // redirect to home if already logged in
+    this.router.navigate(['/']);
+  }
+}
